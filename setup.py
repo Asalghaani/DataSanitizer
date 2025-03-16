f46e941e28e2ef6ee65c97b4beb1b1c04954703f
Mohammad Khazaei <mkhazaei.dev@gmail.com>
2025-03-16 07:12:36 +0330

    Create initial settings to help install the library for use in other projects.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DataSanitizer',
+    version='0.1.0',
+    author='',  # Add your name
+    author_email='',  # Add your Email
+    description='',  # Add a brief but complete description of the project.
+    packages=find_packages(),
+    install_requires=[
+        # requirements list
+
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
